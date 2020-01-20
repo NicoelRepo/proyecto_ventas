dfb376fba0db0bf2bbd9ed13c2ab6a117dfcdfcc
NicoelRepo <nicolasrepollo2000@hotmail.com>
2020-01-20 00:31:50 -0300

    Traer del storage pero no guardar todavia

--- clases.py
+++ clases.py
@@ -1,4 +1,7 @@
 class venta:
+    '''
+    tupla_venta = (prenda, precio, modo de pago, nombre de tarjeta, cuotas)
+    '''
 
     def pedir_prenda(self):
         print()
@@ -32,7 +35,7 @@ class venta:
             break	
         return (self.credito_o_debito, self.nombre_targ, self.cuotas)	  
 
-    def __init__(self):
+    def venta_input(self):
         self.prenda = ', '.join(self.pedir_prenda().split()).upper()
         while True:
             try:
@@ -49,9 +52,26 @@ class venta:
                 self.tupla_venta = (self.prenda, self.precio, 'Efectivo', '-', 0)
                 break  
             else:
-                print('Introduzca los datos nuevamente')      
+                print('Introduzca los datos nuevamente')
+
+
+    def venta_manual(self, caract):
+        self.prenda = caract[0]
+        self.precio = int(caract[1])
+        self.modo = caract[2]
+        self.credito_o_debito = caract[3]
+        self.cuotas = int(caract[4])
+        self.tupla_venta = (self.prenda, self.precio, self.modo, self.credito_o_debito, self.cuotas)
 
 class dia():
+    '''
+    fecha = (dia, mes, año) tuple(int)
+    libro_del_dia = {
+        'efectivo' : [tupla_venta1, ... , tupla_ventaN]
+
+        'efectivo' : [tupla_venta1, ... , tupla_ventaN]
+    }
+    '''
 
     def __init__(self, fecha):
         self.fecha = (fecha[0], fecha[1], fecha[2])
@@ -59,7 +79,17 @@ class dia():
 
     def añadir_venta(self):
         venta_aux = venta()
+        venta_aux.venta_input()
         if venta_aux.modo == 'efectivo':
             self.libro_del_dia['efectivo'].append(venta_aux.tupla_venta)
         else:
-            self.libro_del_dia['tarjeta'].append(venta_aux.tupla_venta)   
+            self.libro_del_dia['tarjeta'].append(venta_aux.tupla_venta)
+        
+    def añadir_venta_manual(self, caract):
+        venta_aux = venta()
+        venta_aux.venta_manual(caract)
+        if venta_aux.modo == 'efectivo':
+            self.libro_del_dia['efectivo'].append(venta_aux.tupla_venta)
+        else:
+            self.libro_del_dia['tarjeta'].append(venta_aux.tupla_venta)
+    
