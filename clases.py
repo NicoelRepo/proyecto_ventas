8eaed8ecd19be57267ce27e60f4181016005be61
Nico_Lomas <nicolasrepollo2000@hotmail.com>
2020-01-20 19:27:16 -0300

    Terminado el Storage

--- clases.py
+++ clases.py
@@ -5,50 +5,64 @@ class venta:
 
     def pedir_prenda(self):
         print()
-        self.prenda = str(input('Prenda: '))
+        self.prenda = str(input('[C]ancelar   Prenda: '))
+        if self.prenda.lower() == 'c':
+            raise Exception
         return self.prenda
     
     def pedir_tarjeta_y_cuotas(self):
         tarjetas = ['visa', 'master', 'maestro', 'cabal', 'naranja', 'american express']
         while(True):
-            self.credito_o_debito = str(input('Introduzca credito o debito: ')).lower()
-            if ['credito', 'debito'].count(self.credito_o_debito) != 1:
+            self.credito_o_debito = str(input('[C]ancelar   Introduzca [Cred]ito o [D]ebito: ')).lower()
+            if self.credito_o_debito == 'c':
+                raise Exception
+            if ['cred', 'd'].count(self.credito_o_debito) != 1:
                 print('Introduzca los datos nuevamente')
                 continue 
             break
 
         while(True):
-            self.nombre_targ = str(input('Nombre de tarjeta: ')).lower()
+            self.nombre_targ = str(input('[C]ancelar   Nombre de tarjeta: ')).lower()
+            if self.nombre_targ == 'c':
+                raise Exception
             if tarjetas.count(self.nombre_targ) != 1:
                 print('Introduzca los datos nuevamente')
                 continue    
         
-            if self.credito_o_debito == 'debito':
+            if self.credito_o_debito == 'd':
                 self.cuotas = 0
             else:
                 while True:   
                     try:
-                        self.cuotas = int(input('Cuotas: '))
+                        self.cuotas = input('[C]ancelar   Cuotas: ')
+                        if self.cuotas == 'c':
+                            raise Exception
+                        self.cuotas = int(self.cuotas)    
                         break
                     except ValueError:
                         print('Las cuotas no son validas')    
             break	
         return (self.credito_o_debito, self.nombre_targ, self.cuotas)	  
 
-    def venta_input(self):
-        self.prenda = ', '.join(self.pedir_prenda().split()).upper()
+    def venta_input(self):    
+        self.prenda = ', '.join(self.pedir_prenda().split('  ')).upper()
         while True:
             try:
-                self.precio = int(input('Precio: '))
+                self.precio = input('[C]ancelar   Precio: ')
+                if self.precio.lower() == 'c':
+                    raise Exception
+                self.precio = int(self.precio)    
                 break
             except ValueError:
                 print('El precio es incorrecto')    
         while True:
-            self.modo = str(input('tarjeta o Efectivo: ')).lower()
-            if self.modo == 'tarjeta':
+            self.modo = str(input('[C]ancelar   [T]arjeta o [E]fectivo: ')).lower()
+            if self.modo.lower() == 'c':
+                raise Exception
+            if self.modo == 't':
                 self.tupla_venta = (self.prenda, self.precio,) + self.pedir_tarjeta_y_cuotas()
                 break
-            elif self.modo == 'efectivo':
+            elif self.modo == 'e':
                 self.tupla_venta = (self.prenda, self.precio, 'Efectivo', '-', 0)
                 break  
             else:
@@ -65,7 +79,7 @@ class venta:
 
 class dia():
     '''
-    fecha = (dia, mes, año) tuple(int)
+    fecha = (dia, mes, año) tuple(str)
     libro_del_dia = {
         'efectivo' : [tupla_venta1, ... , tupla_ventaN]
 
@@ -79,16 +93,19 @@ class dia():
 
     def añadir_venta(self):
         venta_aux = venta()
-        venta_aux.venta_input()
-        if venta_aux.modo == 'efectivo':
-            self.libro_del_dia['efectivo'].append(venta_aux.tupla_venta)
-        else:
-            self.libro_del_dia['tarjeta'].append(venta_aux.tupla_venta)
-        
+        try:
+            venta_aux.venta_input()
+            if venta_aux.modo == 'efectivo':
+                self.libro_del_dia['efectivo'].append(venta_aux.tupla_venta)
+            else:
+                self.libro_del_dia['tarjeta'].append(venta_aux.tupla_venta)
+        except Exception:
+            pass
+
     def añadir_venta_manual(self, caract):
         venta_aux = venta()
         venta_aux.venta_manual(caract)
-        if venta_aux.modo == 'efectivo':
+        if venta_aux.modo.lower() == 'efectivo':
             self.libro_del_dia['efectivo'].append(venta_aux.tupla_venta)
         else:
             self.libro_del_dia['tarjeta'].append(venta_aux.tupla_venta)
--- main.py
+++ main.py
@@ -12,26 +12,48 @@ DAYS = {}
 #       fechaN : diaN
 #   ]
 
-def initialize_DAYS_from_storage():
+def _initialize_DAYS_from_storage():
     global DAYS
 
     DAYS_aux = []
-    with open('.DAYS.csv', 'r') as f:
+    with open('.DAYS.csv', mode='r') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             DAYS_aux.append(row)
     
-    dia_actual = (tuple(DAYS_aux[0]))
-    create_new_day(dia_actual)
+    try:
+        dia_actual = (tuple(DAYS_aux[0]))
+        create_new_day(dia_actual)
+    except IndexError:
+        pass    
     for i,value in enumerate(DAYS_aux):
         if i == 0 or DAYS_aux[i-1] == ['-']:
             continue
         if value == ['-']:
-            dia_actual = (tuple(DAYS_aux[i+1]))
-            create_new_day(dia_actual)
-            continue
+            try:
+                dia_actual = (tuple(DAYS_aux[i+1]))
+                create_new_day(dia_actual)
+                continue
+            except IndexError:
+                continue    
 
         DAYS['/'.join(dia_actual)].añadir_venta_manual(DAYS_aux[i])
+
+
+def _save_DAYS_to_storage():
+    with open('.DAYS.csv.tmp', mode='w', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        for day in DAYS:
+            writer.writerow(DAYS[day].fecha)
+            for venta in DAYS[day].libro_del_dia['efectivo']:
+                writer.writerow(venta)
+            for venta in DAYS[day].libro_del_dia['tarjeta']:
+                writer.writerow(venta)
+            writer.writerow('-')
+
+    
+    os.remove('.DAYS.csv')
+    os.rename('.DAYS.csv.tmp', '.DAYS.csv')
 
 
 def create_new_day(day):
@@ -61,6 +83,7 @@ def display_day_edition_menu(dia_a_editar):
         
         if command == 'a':
             dia_a_editar.añadir_venta()
+            _save_DAYS_to_storage()
         
         elif command == 'e':
             try:
@@ -77,6 +100,7 @@ def display_day_edition_menu(dia_a_editar):
                 print('La venta no ha sido encontrada')
             else:
                 print('La venta ha sido eliminada correctamente')
+            _save_DAYS_to_storage()
 
         elif command == 'l':
             print('-----------------*-----------------')
@@ -91,6 +115,7 @@ def display_day_edition_menu(dia_a_editar):
             print('-----------------*-----------------')
 
         elif command == 's':
+            _save_DAYS_to_storage()
             break
 
         else:
@@ -118,6 +143,7 @@ def display_main_menu():
             day = tuple(day_aux.split('/'))
             try:
                 create_new_day(day)
+                _save_DAYS_to_storage()
                 display_day_edition_menu(DAYS[day_aux])
             except (IndexError, KeyError):
                 print('La fecha es incorrecta')
@@ -146,9 +172,9 @@ def display_main_menu():
 
 
 def run():
-    initialize_DAYS_from_storage()
+    _initialize_DAYS_from_storage()
     display_main_menu()
-
+    _save_DAYS_to_storage()
 
 if __name__ == '__main__':
 	run()
